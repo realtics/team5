183f9d3d5fdddb70011c1399018ec01050b7f580
wonhunShin <eeov1056@naver.com>
2019-11-15 17:36:10 +0900

    191115PM5:35
    코드 및 파일 정리
    설명 주석 제외한 주석, 임시 값, 실험 코드 삭제 함.

--- BrawlStars/Assets/Scripts/Map/MoveStage.cs
+++ BrawlStars/Assets/Scripts/Map/MoveStage.cs
@@ -2,68 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MoveStage : MonoBehaviour
 {
 
     public MapGenerator mapGenerator;
     public GameObject ResultUI;
 
+    //지정된 시간을 지나면 종료
     public float LimitTime;
+    
+    //지전된 시간을 저장
     float SelectTime;
 
+    //자동 진행 타임
     public float nextTime;
 
     public bool OnOff = false;
 
-    string secondText;
+    public Text secondText;
 
     // Start is called before the first frame update
     void Start()
     {
-        //ResultUI = GameObject.Find("ResultUI");
         ResultUI.SetActive(false);
         
         mapGenerator.mapIndex = 0;
         SelectTime = LimitTime;
-
-        nextTime = LimitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        secondText.text = Mathf.Round(nextTime) + " 초 뒤 자동 진행";
+
         if (OnOff)
         {
             if (LimitTime > 0)
             {
-                Mathf.Round(LimitTime);
                 LimitTime -= Time.deltaTime;
             }
             else
             {
+                //결과창 켬
                 ResultUI.SetActive(true);
 
+                //자동 진행 타임
                 if (nextTime > 0)
                 {
                     nextTime -= Time.deltaTime;
                 }
                 else
-                    OnNextBttonClick();
+                    NextMap();
 
-                //if (mapGenerator.mapIndex < mapGenerator.maps.Length - 1)
-                //    mapGenerator.mapIndex++;
-                //else
-                //    mapGenerator.mapIndex = 0;
-
-                //LimitTime = SelectTime;
-
-                //mapGenerator.LoadMap();
             }
         }
     }
 
-    public void OnNextBttonClick()
+    public void NextMap()
     {
         if (mapGenerator.mapIndex < mapGenerator.maps.Length - 1)
             mapGenerator.mapIndex++;
@@ -72,8 +68,7 @@ public class MoveStage : MonoBehaviour
 
         LimitTime = SelectTime;
 
-        nextTime = SelectTime;
-
+        //결과창 끄고 다음 인덱스 로딩
         ResultUI.SetActive(false);
         mapGenerator.LoadMap();
     }
@@ -85,5 +80,6 @@ public class MoveStage : MonoBehaviour
         mapGenerator.LoadMap();
     }
 
+    
 
 }
