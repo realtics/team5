5d9d3edaf11a5b15afc166e2dedfff5d665e90ab
wonhunShin <eeov1056@naver.com>
2019-11-20 18:31:55 +0900

    191120PM6:31
    스타팅 포인트 완료.
    아이템 드롭과 습득 제작 중

---
+++ BrawlStars/Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+{
+    private Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Item")
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
